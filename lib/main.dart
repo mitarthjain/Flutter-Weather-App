97aac9dc3028bfc3318c5948d65371487d25f5c9
mitarthjain <34244395+mitarthjain@users.noreply.github.com>
2019-03-18 21:14:38 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import './ui/klimatic.dart';
+void main()
+{
+  runApp(
+    new MaterialApp(
+      title: 'GDG',
+      home: new Klimatic(),
+    )
+  );
+}
+
+
+
---
+++ lib/util/utils.dart
@@ -0,0 +1,2 @@
+final appId = "0b7be415ac1fcb476499ab02986c7a7d";
+final defaultCity = "Vellore";
